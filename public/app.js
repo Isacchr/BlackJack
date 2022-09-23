6c2b1fdac8a356942218c13a6ed26d13e691401b
isacchr <isac.christiansson@gmail.com>
2022-09-23 15:03:48 +0200

    tried to write if card is hearts or spade with if statements but did not work

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ var yourCards = [];
 
 build = () => {
 
-    
+
 
 }
 
@@ -15,18 +15,42 @@ startGame = () => {
 
     yourCards = [];
 
-    var randomizer = (max) => { 
+    var randomizerCardDenomination= (max) => { 
+        return Math.floor(Math.random(cardDeck) * max)
+    }
+    
+    var randomizerCardType = (max) => { 
         return Math.floor(Math.random(cardDeck) * max)
     }
 
     for ( i=0; i<2; i++) {
 
-        var cards = (randomizer(4) + "," + randomizer(14));
+        var cards = (randomizerCardDenomination(4) + "," + randomizerCardType(13));
+
         yourCards.push(cards); 
+
+        if ( x == 0 ) {
+            console.log('hjärter');
+        } 
 
     }
 
+
+    var playerCards = document.getElementById('playerCards');
+    playerCards.innerHTML = yourCards;
+    
     console.log(yourCards);
+
+    /* if ( [x] == 0 ) {
+        console.log('hjärter');
+    };
+
+    if ( [x] == 1 ) {
+        console.log('klöver');
+    }; */
+
+    //help(yourCards);
+
 
     const divPlayingField = document.getElementById('playingField');
     const btnStartGame = document.getElementById('btnStartGame');
@@ -36,15 +60,38 @@ startGame = () => {
 
 }
 
+/* help = (yourCards) => {
+
+    if( [x] == 0 ) {
+        console.log('hjärter');
+    }
+
+    if( [x] == 1 ) {
+        console.log('klöver');
+    }
+
+    if( [x] == 2 ) {
+        console.log('spader');
+    }
+
+    if( [x] == 3 ) {
+        console.log('diamant');
+    }
+
+} */
+
 hit = () => {
 
     var randomizer = (max) => { 
         return Math.floor(Math.random(cardDeck) * max)
     }
 
-    var cards = (randomizer(4) + "," + randomizer(14));
+    var cards = (randomizer(4) + "," + randomizer(13));
     yourCards.push(cards); 
 
     console.log(yourCards);
+
+    /*var playerCards = document.getElementById('playerCards');
+    playerCards.innerHTML = yourCards;*/
 
 }
