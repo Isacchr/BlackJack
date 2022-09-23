c4d5d76d0ba5900588d49fbdadf180a45876913c
isacchr <isac.christiansson@gmail.com>
2022-09-23 12:43:09 +0200

    refactored my arrays for cardDeck and added random, random not connected to array yet

--- public/app.js
+++ public/app.js
@@ -1,17 +1,32 @@
-console.log('hey from js');
 
-const deck = [
-    [1,2,3,4,5,6,7,8,9,10,11,12,13],
-    [1,2,3,4,5,6,7,8,9,10,11,12,13],
-    [1,2,3,4,5,6,7,8,9,10,11,12,13],
-    [1,2,3,4,5,6,7,8,9,10,11,12,13]
-];
+var cardDeck = new Array();
 
-console.log(deck[1][4]);
+    cardDeck[0] = new Array(13);
+    cardDeck[1] = new Array(13); 
+    cardDeck[2] = new Array(13); 
+    cardDeck[3] = new Array(13); 
 
+    
 build = () => {
 
+    //console.log(cardDeck[1][4]);
 
+}
+
+startGame = () => {
+
+    const min = Math.ceil(cardDeck[0][0]);
+    const max = Math.floor(cardDeck[3][12]);
+
+    var getRandomCard = Math.floor(Math.random() * max + min);
+
+    console.log(getRandomCard);
+
+    const divPlayingField = document.getElementById('playingField');
+    const btnStartGame = document.getElementById('btnStartGame');
+
+    divPlayingField.hidden = false;
+    btnStartGame.innerHTML = 'Start New Game';
 
 }
 
