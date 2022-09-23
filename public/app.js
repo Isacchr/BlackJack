402417f851f4ea53b6757bbdc297cc4a993627a1
isacchr <isac.christiansson@gmail.com>
2022-09-23 13:42:16 +0200

    added new array that saves players card and array resets when you start new game

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ var y;
 
 var cardDeck = [x,y];
 
+var yourCards = [];
 
 build = () => {
 
@@ -11,20 +12,28 @@ build = () => {
 
 startGame = () => {
 
+    yourCards = [];
+
     var randomizer = (max) => { 
         return Math.floor(Math.random(cardDeck) * max)
     }
 
-    console.log(randomizer(4) + "," + randomizer(14));
+    for ( i=0; i<2; i++) {
 
+        var cards = (randomizer(4) + "," + randomizer(14));
+        yourCards.push(cards); 
 
+    }
 
+    console.log(yourCards);
 
     const divPlayingField = document.getElementById('playingField');
     const btnStartGame = document.getElementById('btnStartGame');
 
     divPlayingField.hidden = false;
     btnStartGame.innerHTML = 'Start New Game';
+
+
 
 }
 
